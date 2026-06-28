45417c899a1efef50febd9839f70ee81de608d23
Vijay Anandh <vijay.anandh@globalizeme.com>
2026-06-28 15:37:56 +0530

    Fix s3 image 404 issue

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
         hostname: '*.s3.ap-south-1.amazonaws.com',
       },
     ],
+    // Must be less than DOWNLOAD_TTL (86400 s) so Next.js always re-fetches
+    // the optimized image before the presigned S3 URL expires.
+    minimumCacheTTL: 82800,
   },
 };
 
