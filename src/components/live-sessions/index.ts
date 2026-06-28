fd480520734cc2e1d7aa50af9f143eaec61db535
Vijay Anandh <vijay.anandh@globalizeme.com>
2026-06-28 19:46:31 +0530

    Fix build issue

--- src/components/live-sessions/index.ts
+++ src/components/live-sessions/index.ts
@@ -1,3 +1,1 @@
 export { default as LiveSessionsSection } from './LiveSessionsSection';
-export type { LiveSession, LiveSessionsSectionProps } from './LiveSessionsSection';
-export { defaultLiveSessionsContent } from './defaultLiveSessionsContent';
